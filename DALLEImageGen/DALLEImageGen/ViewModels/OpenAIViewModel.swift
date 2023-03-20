591571b4cdb2e69a1b12062d74da8a27a7fcc58c
Jaehoon-So <jhspacelover@naver.com>
2023-03-20 19:13:41 +0900

    [Feat] ViewModel

---
+++ DALLEImageGen/DALLEImageGen/ViewModels/OpenAIViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  OpenAIViewModel.swift
+//  DALLEImageGen
+//
+//  Created by Jaehoon So on 2023/03/20.
+//
+
+import UIKit
+
+import OpenAIKit
+
+final class OpenAIViewModel {
+    private var openai: OpenAI?
+    
+    func setup() {
+        let openai = OpenAI(
+            Configuration(
+                organizationId: "January1st",
+                apiKey: "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"
+            )
+        )
+    }
+    
+    func generateImage(prompt: String) async -> UIImage? {
+        guard let openai = openai else { return nil }
+        
+        do {
+            let params = ImageParameters(
+                prompt: prompt,
+                resolution: .medium,
+                responseFormat: .base64Json
+            )
+            let result = try await openai.createImage(parameters: params)
+            let data = result.data[0].image
+            let image = try openai.decodeBase64Image(data)
+            
+            return image
+        } catch {
+            print(String(describing: error))
+            return nil
+        }
+    }
+}
--- DALLEImageGen/DALLEImageGen/Views/PromptViewController.swift
+++ DALLEImageGen/DALLEImageGen/Views/PromptViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PromptViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
